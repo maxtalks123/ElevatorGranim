2cf71762d243339366d0f6d4e0591000202361cb
Max Emerson <maxemerson2013@gmail.com>
2024-03-13 14:06:37 +0000

    minor changes to granimjs file

--- granim.js
+++ granim.js
@@ -1,20 +1,20 @@
-let granimInstance = new Granim({
-  element: "#granimbg",
-  direction: "top-bottom",
-  isPausedWhenNotInView: true,
-  image: {
-    source: "elevator.jpeg",
-    blendingMode: "multiply",
-  },
-  states: {
-    "default-state": {
-      gradients: [
-        ["#29323c", "#485563"],
-        ["#FF6B6B", "#556270"],
-        ["#80d3fe", "#7ea0c4"],
-        ["#f0ab51", "#eceba3"],
-      ],
-      transitionSpeed: 7000,
-    },
-  },
-});
+// let granimInstance = new Granim({
+//   element: "#granimbg",
+//   direction: "top-bottom",
+//   isPausedWhenNotInView: true,
+//   image: {
+//     source: "elevator.jpeg",
+//     blendingMode: "multiply",
+//   },
+//   states: {
+//     "default-state": {
+//       gradients: [
+//         ["#29323c", "#485563"],
+//         ["#FF6B6B", "#556270"],
+//         ["#80d3fe", "#7ea0c4"],
+//         ["#f0ab51", "#eceba3"],
+//       ],
+//       transitionSpeed: 7000,
+//     },
+//   },
+// });
